e58d50edf16a5cb38d62521b1a5fce398b0cd392
wukai <1183323265@qq.com>
2015-11-27 19:25:06 +0800

    2015-11-27 commit

--- astar-path-finding/index.js
+++ astar-path-finding/index.js
@@ -1,7 +1,8 @@
-var size = 20;
+var size = 15;
 
 var map = [];
 var blocks = [];
+var wallFrequency = 0.1;
 
 var canvas = document.getElementById("drawing");
 var context = canvas.getContext("2d");
@@ -9,39 +10,49 @@ var context = canvas.getContext("2d");
 var gridWidth = Math.floor((canvas.width -size) / size);
 var gridHeight = Math.floor((canvas.width -size) / size);
 
+var mapHeight = size*(gridHeight+1);
+var mapWidth = size*(gridWidth+1);
+
 var xSelect = 0, ySelect = 0;
 
 var blockReady = false;
 var pathChanged = true;
-
+//var start = new GraphNode(0, 0, 0);
+//var end = new GraphNode(Math.floor(Math.random()*size), Math.floor(Math.random()*size), 0);
+var start;
+var end;
 
 function init() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw vertical lines
-    for (var i = 0; i < size+1; i++) {
-        context.moveTo(i * (gridWidth + 1), 0);
-        context.lineTo(i * (gridWidth + 1), size*(gridHeight+1));
+    for (var x = 0; x < size; x++) {
+        var row = [];
+        for (var y = 0; y < size; y++) {
+            var newGrid = new GraphNode(x, y, 0);
+            row.push(newGrid);
+           //if ((x !== start.x || y !== start.y) && (x !== end.x || y !== end.y)) {
+            //if (start.pos !== newGrid.pos && end.pos !== newGrid.pos) {
+                if (Math.random() <  wallFrequency ) {
+                    row[y].type = 1;
+                    blocks.push(newGrid);
+               // }
+            }
+        }
+        map.push(row);
     }
-    context.stroke();
 
-    // Draw horizontal lines
-    for (i = 0; i < size+1; i++) {
-        context.moveTo(0, i * gridWidth + i);
-        context.lineTo(size*(gridWidth+1), i * gridWidth + i);
+    start = map[0][0];
+    end = map[Math.floor(Math.random()*size)][Math.floor(Math.random()*size)];
+    start.type = 0;
+    end.type = 0;
+    if (blocks.findGraphNode(start)) {
+        blocks.removeGraphNode(start);
     }
-    context.stroke();
-
-    // TODO: addRandomBlocks();
-}
-
-
-for (var x = 0; x < size; x++) {
-    var row = [];
-    for (var y = 0; y < size; y++) {
-        row.push(new GraphNode(x, y, 0));
+    if (blocks.findGraphNode(end)) {
+        blocks.removeGraphNode(end);
     }
-    map.push(row);
+
+    var path = astar.search(map, start, end);
+
 }
 
 init();
@@ -53,17 +64,10 @@ requestAnimFrame = (function() {
         window.oRequestAnimationFrame ||
         window.msRequestAnimationFrame ||
         function(callback) {
-            window.setTimeout(callback, 1000/60);
-
+            window.setTimeout(callback, 1000/30);
         };
 })();
 requestAnimFrame(draw);
-
-var start = new GraphNode(0, 0, 0);
-var end = new GraphNode(Math.floor(Math.random()*size), Math.floor(Math.random()*size), 0);
-console.log(end.pos.x, end.pos.y);
-var path = astar.search(map, start, end);
-
 
 function drawPath() {
     fillGrid(start.x, start.y, "red");
@@ -105,11 +109,18 @@ function mouseMove(e) {
 }
 
 function onClick(e) {
-    if(blockReady == true) {
-        map[xSelect][ySelect].type = 1;
-        // blockReady = false;
-        var newBlock = new GraphNode(xSelect, ySelect, 1);
-        blocks.push(newBlock);
+    if(blockReady) {
+        if (map[xSelect][ySelect].type === 0) {
+            map[xSelect][ySelect].type = 1;
+
+            blocks.push(map[xSelect][ySelect]);
+        } else if(map[xSelect][ySelect].type === 1) {
+            if (blocks.findGraphNode(map[xSelect][ySelect])) {
+                blocks.removeGraphNode(map[xSelect][ySelect]);
+                map[xSelect][ySelect].type = 0;
+            }
+        }
+
         //if (newBlock in path) {
             pathChanged = true;
         //}
@@ -124,20 +135,40 @@ function placeBlock() {
 function draw() {
     requestAnimFrame(draw);
 
-    init();
+    drawBoard();
 
     // TODO: only when path changed, search the path again
     if (pathChanged) {
         path = astar.search(map, start, end);
         pathChanged = false;
     }
+
     drawPath();
-    drawBlocks();
-    if (blockReady == true) {
-        fillGrid(xSelect, ySelect, "black");
+
+    if (blockReady) {
+        fillGrid(xSelect, ySelect, "gray");
     }
 
+    drawBlocks();
 
+}
+
+function drawBoard() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.beginPath();
+    // Draw vertical lines
+    for (var i = 0; i < size+1; i++) {
+        context.moveTo(i * (gridWidth + 1), 0);
+        context.lineTo(i * (gridWidth + 1), mapHeight);
+    }
+
+    // Draw horizontal lines
+    for (i = 0; i < size+1; i++) {
+        context.moveTo(0, i * gridWidth + i);
+        context.lineTo(mapWidth, i * gridWidth + i);
+    }
+    context.stroke();
 }
 
 
